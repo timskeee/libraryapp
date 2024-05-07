fa3e72b487009ec5d7f05c49773f11b2e0dfe089
timskeee <timskee@gmail.com>
2024-05-07 07:58:03 -0700

    added sessions

--- catalog/views.py
+++ catalog/views.py
@@ -15,6 +15,10 @@ def index(request):
     # The 'all()' is implied by default.
     num_authors = Author.objects.count()
 
+    # Number of visits to this view, as counted in the session variable. If not previously set, set value to 0
+    num_visits = request.session.get('num_visits', 0)
+    request.session['num_visits'] = num_visits + 1
+
     context = {
         'num_books': num_books,
         'num_instances': num_instances,
